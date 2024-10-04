0c8d9f2112f0a32ce34d53dbaea38eddf02e708a
hydarps <116024971+hydarps@users.noreply.github.com>
2024-10-05 00:16:28 +0200

    Create einstein.py

---
+++ week0/pset0/einstein.py
@@ -0,0 +1,5 @@
+mass = int(input("enter mass:")) #ask for a number input then convert to int to perform a matematical operation
+emc = (3 * 10**8)**2 #E=mc2
+calc = (mass*emc) #calculate mass and e=mc2
+
+print(calc)
