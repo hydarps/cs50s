43755cd7032e163c3afa9256ffe12414dec03a29
hydarps <116024971+hydarps@users.noreply.github.com>
2024-10-05 00:19:42 +0200

    Create deep2.py

---
+++ week1/pset1/deep2.py
@@ -0,0 +1,6 @@
+question = input("What is the answer to the great question in life, the univerese, everything? ").lower()
+
+if question != "42" and question != "forty two" and question != "forty_two":
+    print("No")
+else:
+    print("yes")
