658cb16dafece2fd5a4955421a646be9f11039fe
hydarps <116024971+hydarps@users.noreply.github.com>
2024-10-14 22:16:19 +0200

    Update coke_machine.py

--- week2/pset2/coke_machine.py
+++ week2/pset2/coke_machine.py
@@ -1,17 +1,13 @@
 amount_due = 50
-still_due = 0
 
-while still_due <= amount_due:
+while amount_due > 0:
+    print(f"Amount Due: {amount_due}")
+
     coin = int(input("Insert coin: "))
     if coin in [5, 10, 25, 50]:
-        still_due += coin
-        print(f"Insert more coin: {amount_due - still_due}")
-        if still_due == amount_due:
-            break
-        else:
-            (still_due + coin) >= amount_due
-            change = (still_due + coin) - amount_due
+        amount_due -= coin
     else:
-        print("Insert 5, 10, 25 or 50 cents only")
+        print("Amount Due: ")
 
-print(f"Balance paid, your change {change - amount_due}")
+change_owed = abs(amount_due)
+print(f"Change Owed: {change_owed}")
